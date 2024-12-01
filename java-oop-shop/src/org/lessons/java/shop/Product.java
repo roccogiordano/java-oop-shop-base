7374209592a6a3140d72fc00e0a73a2d3d455ae5
roccogiordano <rocco.giordano573@gmail.com>
2024-12-01 23:34:04 +0100

    feat: added Product class methods

--- java-oop-shop/src/org/lessons/java/shop/Product.java
+++ java-oop-shop/src/org/lessons/java/shop/Product.java
@@ -17,4 +17,38 @@ public class Product {
 
     //
 
+
+    // Product Methods
+
+    Random randomCode = new Random();
+
+    public Product (String name, String description, float basePrice, int iva) {
+
+        this.code = randomCode.nextInt(999999);
+
+        this.name = name;
+
+        this.description = description;
+
+        this.basePrice = basePrice;
+
+        this.iva = iva;
+
+        this.totalPrice = basePrice + (basePrice / 100 * iva);
+
+    }
+
+    public String getBasePrice() {
+        return("Price before taxes is: " + basePrice);
+    }
+
+    public String getTotalPrice() {
+        return("Price after taxes will be: " + totalPrice);
+    }
+
+    public String getFullName() {
+        return("Product full name is: " + this.code + "-" + this.name);
+    }
+
+    //
 }
