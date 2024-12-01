ace461e35547ee778221807422fc2e8a74951c85
roccogiordano <rocco.giordano573@gmail.com>
2024-12-01 23:31:14 +0100

    feat: added Product class with instance variables

---
+++ java-oop-shop/src/org/lessons/java/shop/Product.java
@@ -0,0 +1,20 @@
+package org.lessons.java.shop;
+
+import java.util.Random;
+
+// Class Definition
+
+public class Product {
+
+    // Instance Variables Declaration
+
+    int code;
+    String name;
+    String description;
+    float basePrice;
+    float totalPrice;
+    int iva;
+
+    //
+
+}
