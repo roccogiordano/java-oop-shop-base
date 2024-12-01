a2df72daaba54118251ab9116a7ac1fc911ccf86
roccogiordano <rocco.giordano573@gmail.com>
2024-12-01 23:35:07 +0100

    feat: added Main with Product instances

---
+++ java-oop-shop/src/org/lessons/java/shop/Main.java
@@ -0,0 +1,15 @@
+package org.lessons.java.shop;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Product Instances
+
+        Product firstProduct = new Product("Lemons", "Sorrento Lemons!", 5, 8);
+
+        Product secondProduct = new Product("Ice Cream", "Chocolate Flavoured Ice Cream!", 8, 22);
+
+        //
+        
+    }
+}
